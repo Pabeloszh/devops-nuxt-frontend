5e170812fa77946cbccbc4ba1ca2d232f46215e5
Pawel Mielech <pawel.mielech@atos.net>
2025-05-28 16:24:07 +0200

    feat: check container functionality after dependencies update

--- tests/HelloWorld.spec.ts
+++ tests/HelloWorld.spec.ts
@@ -1,6 +1,8 @@
 import { describe, it, expect } from 'vitest'
+import { parse } from 'date-fns'
 
 describe('HelloWorld', () => {
+  console.log(parse('02/11/2014', 'MM/dd/yyyy', new Date()))
   it('is true truthy', () => {
     expect(true).toBeTruthy()
   })
